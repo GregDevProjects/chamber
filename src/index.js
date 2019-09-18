e60e4dc9f880d8cb9666d5c2e81749e0c201e9c8
Greg McLean <greg@Gregs-MacBook-Pro.local>
2019-09-17 22:57:04 -0300

    bullets

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import Phaser from 'phaser'
 import logoImg from './assets/logo.png'
+import PhaserMatterCollisionPlugin from 'phaser-matter-collision-plugin'
 // baaaad
 let Matter = Phaser.Physics.Matter
 
@@ -24,6 +25,15 @@ const config = {
         y: 0
       }
     }
+  },
+  plugins: {
+    scene: [
+      {
+        plugin: PhaserMatterCollisionPlugin, // The plugin class
+        key: 'matterCollision', // Where to store in Scene.Systems, e.g. scene.sys.matterCollision
+        mapping: 'matterCollision' // Where to store in the Scene, e.g. scene.matterCollision
+      }
+    ]
   }
 }
 
@@ -39,14 +49,55 @@ function create () {
 
   const player = Matter.add.rectangle(250, 250, 50, 50)
 
+  const cat1 = Matter.world.nextCategory()
+  const cat2 = Matter.world.nextCategory()
+
+  player.collisionFilter.group = -1
+
   this.input.on('pointerdown', (coords) => {
-    const force = 0.1
+    const force = 0.04
     const mouseVector = {
       x: coords.worldX,
       y: coords.worldY
     }
 
     applyForceInOppositeDirection(player, mouseVector, force)
+    shoot(player, mouseVector)
+  })
+
+  this.matterCollision.addOnCollideStart({
+    objectA: player,
+    // objectB: trapDoor,
+    callback: function (eventData) {
+      console.log(eventData)
+      // This function will be invoked any time the player and trap door collide
+      // const { bodyA, bodyB, gameObjectA, gameObjectB, pair } = eventData;
+      // bodyA & bodyB are the Matter bodies of the player and door respectively
+      // gameObjectA & gameObjectB are the player and door respectively
+      // pair is the raw Matter pair data
+    },
+    context: this // Context to apply to the callback function
+  })
+}
+
+const shoot = (player, mouseVector) => {
+  const force = 0.003
+  const circle = Matter.add.circle(player.position.x, player.position.y, 8)
+  circle.restitution = 2
+  circle.collisionFilter.group = -1
+
+  let targetAngle = Phaser.Math.Angle.Between(
+    mouseVector.x,
+    mouseVector.y,
+    player.position.x,
+    player.position.y
+  )
+
+  targetAngle = targetAngle - Math.PI
+
+  Matter.body.applyForce(circle, { x: circle.position.x, y: circle.position.y }, {
+    x: Math.cos(targetAngle) * force,
+    y: Math.sin(targetAngle) * force
   })
 }
 
